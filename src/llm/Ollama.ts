9be398cc0930c06d5d1e8e8c5239530ac66a39da
SirSimon04 <simi@engelnetz.de>
2025-01-28 21:40:23 +0100

    feat(llm): add Ollama LLM implementation.

---
+++ src/llm/Ollama.ts
@@ -0,0 +1,24 @@
+import { PromptBuilder } from "../PromptBuilder";
+import { LLM } from "./LLM";
+import ollama from "ollama";
+
+export class Ollama implements LLM {
+  async call(diff: string): Promise<string> {
+    const response = await ollama.chat({
+      model: "llama3.1",
+      messages: [
+        {
+          role: "system",
+          content: new PromptBuilder().getCommitMessageSystemPrompt(),
+        },
+        {
+          role: "user",
+          content:
+            "This is the diff where the commit message needs to be created for: \n \n" +
+            diff,
+        },
+      ],
+    });
+    return response.message.content;
+  }
+}
