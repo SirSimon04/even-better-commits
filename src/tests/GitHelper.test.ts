336166a4cf520bb1b040139ce4075f9a3af66c5f
stefanrudi2004 <stefanrudi2004@gmail.com>
2025-02-03 12:53:00 +0100

    test(tools): Add Jest tests and configuration

---
+++ src/tests/GitHelper.test.ts
@@ -0,0 +1,49 @@
+import { GitHelper } from '../GitHelper'; // Adjust the path as needed
+import { CommitParts } from '../GitHelper'; // Adjust the path as needed
+
+// type CommitParts = {
+//     type: string;
+//     scope?: string;
+//     message: string;
+// };
+
+describe('parseCommitMessage', () => {
+  it('should parse a valid commit message with type, scope, and message', () => {
+    const commitMessage: string = "feat(app): Add new user authentication feature";
+    const expected: CommitParts = {
+      type: "feat",
+      scope: "app",
+      message: "Add new user authentication feature",
+    };
+    expect(GitHelper.parseCommitMessage(commitMessage)).toEqual(expected);
+  });
+
+  it('should parse a valid commit message with type, scope, and message that include whitespaces', () => {
+    const commitMessage: string = "  feat(app): Add new user authentication feature  ";
+    const expected: CommitParts = {
+      type: "feat",
+      scope: "app",
+      message: "Add new user authentication feature",
+    };
+    expect(GitHelper.parseCommitMessage(commitMessage)).toEqual(expected);
+  });
+
+  it('should parse a valid commit message with type and message only', () => {
+    const commitMessage = "fix: Correct typo in error message";
+    const expected: CommitParts = {
+      type: "fix",
+      message: "Correct typo in error message",
+    };
+    expect(GitHelper.parseCommitMessage(commitMessage)).toEqual(expected);
+  });
+
+  it('should return empty strings for an invalid commit message format', () => {
+    const commitMessage = "This is an invalid commit message";
+    const expected: CommitParts = {
+      type: "",
+      scope: "",
+      message: "",
+    };
+    expect(GitHelper.parseCommitMessage(commitMessage)).toEqual(expected);
+  });
+});
