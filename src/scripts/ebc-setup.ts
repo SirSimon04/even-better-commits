454ce968d49c85415f7fa790ccd9012f5568f668
SirSimon04 <simi@engelnetz.de>
2025-02-14 14:56:03 +0100

    chore(tools): Set environment variables

--- src/scripts/ebc-setup.ts
+++ src/scripts/ebc-setup.ts
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
+process.env.SAP_CLOUD_SDK_LOG_LEVEL = "production";
+process.env.NODE_ENV = "error";
+
 import { setup } from "../setup";
 import { intro, outro } from "@clack/prompts";
 
