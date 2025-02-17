4e808c50faa7050adaa25c829b2bd7bff9bff571
SirSimon04 <simi@engelnetz.de>
2025-02-17 17:39:28 +0100

    feat(tools): Add branch creation scripts

--- src/scripts/ebc-branch.ts
+++ src/scripts/ebc-branch.ts
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
-//process.env.SAP_CLOUD_SDK_LOG_LEVEL = "production";
-//process.env.NODE_ENV = "error";
-//
-//// Suppress console logs
-//console.log = () => {};
-//console.error = () => {};
-//console.warn = () => {};
+process.env.SAP_CLOUD_SDK_LOG_LEVEL = "production";
+process.env.NODE_ENV = "error";
+
+// Suppress console logs
+console.log = () => {};
+console.error = () => {};
+console.warn = () => {};
 
 import { createBranch } from "../branch";
 import { clearScreen, restoreScreen } from "../utils";
